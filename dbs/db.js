bc81cbecd1ce13d0e1795e843b0939cb1fa694cb
fatou51 <fc5793138@gmail.com>
2025-08-14 11:59:40 +0200

    mon premier commi

--- dbs/db.js
+++ dbs/db.js
@@ -3,7 +3,7 @@ require("dotenv").config({path:"./config/.env"})
 
 exports.db=async ()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.MONGO_URL1)
         console.log("connexion reussi avec sucée")
     }catch{
         console.log("echec de connexion")
